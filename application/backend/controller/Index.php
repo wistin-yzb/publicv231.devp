d47eca4a66fa06d760e244cd9a8cbe84dcc603ef
git <1391930327@qq.com>
2018-12-15 18:04:48 +0800

    this is a ver2 commit by zoey 2018/12/15 18:05

--- application/backend/controller/Index.php
+++ application/backend/controller/Index.php
@@ -3,12 +3,19 @@
 namespace app\backend\controller;
 
 use \think\View;
-use think\Controller;
+use \think\Controller;
+use think\Db;
 
 class Index extends controller {
 	
 	// index
-	public function index() {
+	public function index() {		
+		$data = Db::name('test')->where('id',1)->find();
+		//助手函数
+// 		$data = db('test')->where('id',1)->find();
+// 		echo '<pre>';
+// 		var_export($data);
+
 		$view = new View ();
 		return $view->fetch ();
 	}
@@ -17,6 +24,26 @@ class Index extends controller {
 	public function login() {
 		$view = new View ();
 		return $view->fetch ();
+	}
+	
+	//checklogin
+	public function checklogin() {
+		$account = input ( 'post.account' );
+		$password = input ( 'post.password' );
+		if ($account == "" || $password == "") {
+			$this->error ( '请填写登录信息' );
+			exit ();
+		}
+		if($account!='fans2019admin'&&$password!='fans2019passwd'){
+			$this->error ( '登录信息填写错误' );
+			exit ();
+		}
+		$captcha = input ( 'post.captcha' );
+		if (! captcha_check ( $captcha )) {
+			$this->error ( '验证码错误' );
+			exit ();
+		}
+		$this->redirect ( 'backend/index/index');
 	}
 	
 	// welcome
--- runtime/temp/403a529e2efc35791ebfd61877fca809.php
+++
@@ -1,2 +0,0 @@
-<?php if (!defined('THINK_PATH')) exit(); /*a:1:{s:135:"F:\xampp-win32-5.6.3-0-VC11-installerroot\xammp\htdocs\www\www.public.devp\public/../application/backend\view\product\product_show.html";i:1544771505;}*/ ?>
-产品展示
